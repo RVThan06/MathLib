6c5e6ae5a0bb7b0f8e2e0370e3c7271ad411f233
RVThan06 <arvinthevar06@gmail.com>
2026-02-17 15:49:54 +0800

    Add Integration test

---
+++ tests/IntegrationTests.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+
+#include <mathlib\numerical\Integration.hpp>
+
+TEST(IntegrationTests, Trapezoidal) {
+    auto poly_func = [](double x) {
+        return std::pow(x, 4) + (static_cast<double>(3.0) * std::pow(x, 3)) + static_cast<double>(5.0) * x;
+    };
+
+    double solution = mathlib::numerical::trapezoidal_integral(poly_func, 2.0, 5.0, 1000);
+    EXPECT_NEAR(solution, 1127.850493, 1e-5);
+}
+
+TEST(IntegrationTests, Simpson_1_3) {
+    auto poly_func = [](double x) {
+        return std::pow(x, 4) + (static_cast<double>(3.0) * std::pow(x, 3)) + static_cast<double>(5.0) * x;
+    };
+
+    double solution = mathlib::numerical::simpson_one_three(poly_func, 2.0, 5.0, 1000);
+    EXPECT_NEAR(solution, 1126.775985, 1e-5);
+}
+
+TEST(IntegrationTests, Simpson_3_8) {
+    auto poly_func = [](double x) {
+        return std::pow(x, 4) + (static_cast<double>(3.0) * std::pow(x, 3)) + static_cast<double>(5.0) * x;
+    };
+
+    double solution = mathlib::numerical::simpson_three_eight(poly_func, 2.0, 5.0, 1000);
+    EXPECT_NEAR(solution, 1127.85, 1e-5);
+}
